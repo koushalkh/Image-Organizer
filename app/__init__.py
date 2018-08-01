71f6dbc1b048f98bfd6b1fda7e2a83fcfec2bb8c
chetanrns <chetanrns1997@gmail.com>
2018-08-01 16:34:27 +0530

    added eb

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,12 @@ from werkzeug.utils import secure_filename
 import os
 
 
+UPLOAD_FOLDER = '/home/chetan/Desktop/Image-Organizer/app/static'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+app=Flask(__name__)
+app.config.from_object(Config)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
