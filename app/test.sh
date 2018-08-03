ff3694da2babe3394fcd5297997f7ce2bf10d6da
chetanrns <chetanrns1997@gmail.com>
2018-08-03 09:53:11 +0530

    added tiny yolo

--- app/test.sh
+++ app/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/chetan/Desktop/darknet
-./darknet detect cfg/yolov3.cfg yolov3.weights $1 > $2
+#cd /home/chetan/Desktop/darknet
+cd /home/Ubuntu/darknet
+./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights $1 > $2
