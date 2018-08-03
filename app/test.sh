122092360a25a2512ccc6c1a7bf47523ba131cf9
chetanrns <chetanrns1997@gmail.com>
2018-08-03 11:18:40 +0530

    added aws

--- app/test.sh
+++ app/test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#cd /home/chetan/Desktop/darknet
-cd /home/Ubuntu/darknet
-./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights $1 > $2
+cd /home/chetan/Desktop/darknet
+./darknet detect cfg/yolov3.cfg yolov3.weights $1 > $2
